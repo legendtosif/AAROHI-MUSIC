10e39fa24a2cfa304880d6843f55d1602f2dca58
legendtosif <toofanxd72@gmail.com>
2024-12-07 21:05:06 +0530

    Update reload.py

--- SHUKLAMUSIC/plugins/tools/reload.py
+++ SHUKLAMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6762113050)
+    & filters.user(7520092354)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
@@ -118,7 +118,7 @@ async def help(client: Client, message: Message):
              [
                  [
                       InlineKeyboardButton(
-                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ •", url=f"https://t.me/SHIVANSHDEVS")
+                         "• 𝖧ᴀᴄᴋᴇᴅ вʏ •", url=f"https://t.me/LEGEND_MICKEY")
                  ]
             ]
          ),
